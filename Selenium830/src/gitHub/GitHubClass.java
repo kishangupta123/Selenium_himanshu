58c594593263e68ec0f33d73e5bd5901cfea8bf7
htiwa <htiwa@Himanshu_PC>
2024-11-15 13:39:22 +0530

    message entered

--- Selenium830/src/gitHub/GitHubClass.java
+++ Selenium830/src/gitHub/GitHubClass.java
@@ -1,5 +1,13 @@
 package gitHub;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class GitHubClass {
+	@Test
+	public void adMessages()
+	{
+		Reporter.log("Welcome To GitHub",true);
+	}
 
 }
